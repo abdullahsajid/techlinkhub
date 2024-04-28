467f394164117419564d72555a4ed9300b02c8d2
Abdullah sajid <abdullahsajid521@gmail.com>
2024-04-28 06:48:06 +0500

    -add stripte api

--- organization/api/projectApis.js
+++ organization/api/projectApis.js
@@ -31,10 +31,11 @@ module.exports = (app) => {
             const data = await projectServices.EditProject(req.params.id,req.body)
             res.status(200).json({
                 data: data,
-                message: 'Project updated successfully!'
+                message: 'Project updated successfully!',
+                success: true
             })
         }catch(err){
-            res.status(500).json({message:err.message})
+            res.status(500).json({message:err.message,success: false})
         }
     })
 
@@ -43,7 +44,20 @@ module.exports = (app) => {
             const data = await projectServices.delProject(req.params.id)
             res.status(200).json({
                 data: data,
-                message: 'Project deleted successfully!'
+                message: 'Project deleted successfully!',
+                success: true
+            })
+        }catch(err){
+            res.status(500).json({message:err.message,success: false})
+        }
+    })
+
+    app.get('/summonAllProjectsByUser',auth, async (req,res) => {
+        try{
+            const data = await projectServices.retrieveProject(req.user.id)
+            res.status(200).json({
+                data: data[0],
+                message: 'Projects retrieved successfully!'
             })
         }catch(err){
             res.status(500).json({message:err.message})
@@ -52,7 +66,7 @@ module.exports = (app) => {
 
     app.get('/summonAllProjects',auth, async (req,res) => {
         try{
-            const data = await projectServices.retrieveProject(req.user.id)
+            const data = await projectServices.retrieveProjects()
             res.status(200).json({
                 data: data[0],
                 message: 'Projects retrieved successfully!'
